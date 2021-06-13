f44f59b92c85e8d7c8ec5cd0a684a78cfa1f90d6
xuanyuansen <wangshuai@wangshuaideMacBook-Pro.local>
2021-06-13 11:25:18 +0800

    update get stock price to standard

--- src/get_stock_price_info.py
+++ src/get_stock_price_info.py
@@ -1,35 +1,47 @@
 # -*- coding:utf-8 -*-
 # remind install clang on mac with cmd, xcode-select --install
 import logging
-
 from MarketPriceSpider.StockInfoSpyder import StockInfoSpyder
-from Utils import config, utils
-# import sys
+from Utils import utils
+import argparse
 
-utils.set_display()
 
-stock_info_spyder = StockInfoSpyder(joint_quant_on=False)
-# stock_info_spyder.get_historical_us_zh_stock_daily_price()
-stock_info_spyder.get_historical_us_stock_daily_price(start_date= '2017-01-01')
-exit(-1)
-# get all name and code
-max_date_hk = stock_info_spyder.db_obj.find_max(
-    config.HK_STOCK_DATABASE_NAME, "00156", "date"
-)
-logging.info("max date is {}".format(max_date_hk))
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-m", "--market", help="which market", required=True, choices=["cn", "hk", "us", "uszh"])
+    parser.add_argument("-s", "--start_date", help="set start date")
+    parser.add_argument("-j", "--joint_quant", help="weather to init quant", default=False)
+    parser.add_argument("-c", "--start_code", help="start code")
+    args = parser.parse_args()
 
-stock_info_spyder.get_historical_hk_stock_daily_price(start_date=max_date_hk, start_symbol='00036')
+    if args.market:
+        logging.info("market type is {}".format(args.market))
+        market_type = args.market
+    else:
+        logging.error('market type unknown!')
+        raise Exception('market type unknown!')
+    if args.start_date:
+        _start_date = args.start_date
+    else:
+        _start_date = None
+    if args.start_code:
+        _start_symbol = args.start_code
+    else:
+        _start_symbol = None
 
-# cn_stock_info_spyder = StockInfoSpyder(joint_quant_on=False)
-max_date_cn = stock_info_spyder.db_obj.find_max(
-    config.STOCK_DATABASE_NAME, "sz002181", "date"
-)
-logging.info("max date cn is {}".format(max_date_cn))
-# hk_stock_info_spyder.get_historical_hk_stock_daily_price(start_symbol='00156')
+    utils.set_display()
 
-# stock_info_spyder.update_cn_stock_money_column_using_joint_quant()
-# stock_info_spyder.get_historical_price_cn_stock(start_date=sys.argv[1])
-# stock_info_spyder.get_all_stock_code_info()
+    # cn_stock_info_spyder = StockInfoSpyder(joint_quant_on=False)
+    stock_info_spyder = StockInfoSpyder(joint_quant_on=args.joint_quant)
 
-# stock_info_spyder.get_today_price()
-pass
+    if "cn" == market_type:
+        stock_info_spyder.get_historical_cn_stock_daily_price(start_date=_start_date)
+    elif "hk" == market_type:
+        stock_info_spyder.get_historical_hk_stock_daily_price(start_date=_start_date,
+                                                              start_symbol=_start_symbol)
+    elif "us" == market_type:
+        # stock_info_spyder.get_historical_us_zh_stock_daily_price()
+        stock_info_spyder.get_historical_us_stock_daily_price(start_date=_start_date)
+    else:
+        stock_info_spyder.get_historical_us_zh_stock_daily_price()
+    pass
